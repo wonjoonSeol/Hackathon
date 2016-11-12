7be162b7930e44d8a6c88de6134c5c66fd4cbf93
Joshua Bradbury <joshbradbury7@hotmail.co.uk>
2016-11-12 13:02:27 +0000

    Added basic main class

---
+++ src/tech/kcl/textadventure/Commands.java
@@ -0,0 +1,14 @@
+package tech.kcl.textadventure;
+
+public class Commands {
+
+	private Main main;
+	
+	public Commands(Main main) {
+		this.main = main;
+	}
+
+	public void runNextCommand() {
+		
+	}
+}
--- src/tech/kcl/textadventure/Main.java
+++ src/tech/kcl/textadventure/Main.java
@@ -1,5 +1,31 @@
 package tech.kcl.textadventure;
 
 public class Main {
-
+	
+	private Commands commands;
+	private boolean running;
+	
+	public Main() {
+		commands = new Commands(this);
+	}
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		
+		main.start();
+	}
+	
+	public void start() {
+		running = true;
+	}
+	
+	public void run() {
+		while (running) {
+			commands.runNextCommand();
+		}
+	}
+	
+	public void stop() {
+		running = false;
+	}
 }

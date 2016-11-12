d30ed48de675f57ed552aec458ed06768fdece7c
freakzilla149 <raihansharif92@gmail.com>
2016-11-12 15:38:10 +0000

    Added name field, setters and getters

--- src/tech/kcl/textadventure/Entity.java
+++ src/tech/kcl/textadventure/Entity.java
@@ -56,6 +56,15 @@ public class Entity {
 	
 	
 	//getters and setters
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
